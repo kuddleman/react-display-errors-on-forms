abb495390947b0dc349c9931ec7b46560b1c1c6c
Kuddleman <edl1155@gmail.com>
2020-06-10 13:07:13 -0700

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+
+  render() {
+    return (
+      <>
+        <p>Hello from Form</p>
+      </>  
+    )
+  }
+}
+
+export default Form
